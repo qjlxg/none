d8377f6c313d4b35bc92365661338483b539ee30
qjlxg <qjlxg@163.com>
2025-03-25 10:46:44 +0800

    Update collect.py

--- utils/airport/collectAirport/subscribe/collect.py
+++ utils/airport/collectAirport/subscribe/collect.py
@@ -144,7 +144,7 @@ def assign(
     # 爬取新站点列表
     if not domains or overwrite:
         candidates = crawl.collect_airport(
-            channel="allv2board",
+            channel="hack_proxy",
             page_num=pages,
             num_thread=num_threads,
             rigid=rigid,
